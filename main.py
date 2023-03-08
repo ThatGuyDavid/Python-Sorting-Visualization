75b268fb0989fe09f47075fe23540bdee6da504d
David Fernandez <dfernandezz951@gmail.com>
2023-03-07 22:51:46 -0800

    Cleaned up code and added bubble sort functionality and visual/ Added funtionality to start, reset, ascend, descending buttons/ Need to complete insertion and merge sort functions

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 import random
+import math
 
 pygame.init()
 
@@ -24,13 +25,10 @@ class DrawApp:
     SIDE_PADDING = 100
     TOP_PADDING = 275
 
-    def __init__(self, height, width, lst, ascending, sorting) -> None:
+    def __init__(self, height, width, lst) -> None:
 
         self.height = height
         self.width = width
-        self.ascending = ascending
-        self.sorting = sorting
-        self.algo_name = "Sort"
 
         # Initialize the window using height and width being passed
         self.window = pygame.display.set_mode((self.width, self.height))
@@ -47,7 +45,7 @@ class DrawApp:
 
         # Make the bars dynamic to the amount in the list stretching ans shrinking using max and min values for proper height difference
         self.bar_width = round((self.width - self.SIDE_PADDING) / len(lst))
-        self.bar_height = round(
+        self.bar_height = math.floor(
             (self.height - self.TOP_PADDING) / (self.max_val - self.min_val)
         )
 
@@ -59,7 +57,13 @@ class Button:
     """ """
 
     def __init__(
-        self, position_x, position_y, width, height, text, on_click_func, draw_app
+        self,
+        position_x,
+        position_y,
+        width,
+        height,
+        text,
+        draw_app,
     ) -> None:
         self.position_x = position_x
         self.position_y = position_y
@@ -67,7 +71,6 @@ class Button:
         self.height = height
         self.text = text
         self.draw_app = draw_app
-        self.on_click_func = on_click_func
         # Check safe for over pressing
         self.pressed = False
         # Color pallete for buttons (normal, hover, pressing)
@@ -88,6 +91,7 @@ class Button:
         self.button_text = self.draw_app.FONT.render(self.text, 1, self.draw_app.WHITE)
 
     def process(self):
+        action = False
         # Get the position of the mouse on the window
         mouse_pos = pygame.mouse.get_pos()
         # Check if mouse overlaps/collides with the button surface/rect
@@ -99,7 +103,7 @@ class Button:
                 (self.position_x, self.position_y, self.width, self.height),
             )
             # Check if button is being pressed down by left-mouse button
-            if pygame.mouse.get_pressed(num_buttons=3)[0]:
+            if pygame.mouse.get_pressed(num_buttons=3)[0] == 1:
                 # If true redraw to indicate pressed profile
                 self.button_surface = pygame.draw.rect(
                     self.draw_app.window,
@@ -109,11 +113,12 @@ class Button:
                 # Check to see if button is currently pressed / active
                 if not self.pressed:
                     # If not active call function and set button parameter to pressed
-                    self.on_click_func()
                     self.pressed = True
+                    action = True
             else:
                 # Unpress button allowing to be repressed
                 self.pressed = False
+                action = False
 
         # Draw words onto buttons/rect
         self.draw_app.window.blit(
@@ -124,21 +129,23 @@ class Button:
             ),
         )
 
+        return action
 
-def draw(draw_app, buttons):
+
+def draw(draw_app, buttons, algo_name, ascending):
     draw_app.window.fill(draw_app.BACKGROUND_COLOR)
-    draw_bars(draw_app)
-    drawing_display(draw_app)
+    drawing_display(draw_app, algo_name, ascending)
     for button in buttons:
         button.draw()
         button.process()
+    draw_bars(draw_app)
     pygame.display.update()
 
 
-def drawing_display(draw_app):
+def drawing_display(draw_app, algo_name, ascending):
     # Create display text
     display_text = draw_app.DISPLAY_FONT.render(
-        f"{draw_app.algo_name} - {'Ascending' if draw_app.ascending else 'Descending'}",
+        f"{algo_name} - {'Ascending' if ascending else 'Descending'}",
         1,
         draw_app.BLACK,
     )
@@ -195,10 +202,6 @@ def draw_bars(draw_app, color_positions={}, clear_bg=False):
         pygame.display.update()
 
 
-def button_press():
-    print("Button Pressed")
-
-
 def create_list(n, min_val, max_val):
     # Create empty list
     lst = []
@@ -211,37 +214,19 @@ def create_list(n, min_val, max_val):
     return lst
 
 
-def new_list(draw_app, n, min_val, max_val):
-    # Assign a new list to draw_app's lst parameter
-    draw_app.lst = create_list(n, min_val, max_val)
-
-
-def bubble_sort(draw_app):
+def bubble_sort(draw_app, ascending):
     lst = draw_app.lst
-    draw_app.algo_name = "Bubble Sort"
 
     for i in range(len(lst) - 1):
         for j in range(len(lst) - 1 - i):
             num1 = lst[j]
             num2 = lst[j + 1]
 
-            if (
-                num1 > num2
-                and draw_app.ascending
-                or (num1 < num2 and not draw_app.ascending)
-            ):
+            if (num1 > num2 and ascending) or (num1 < num2 and not ascending):
                 lst[j], lst[j + 1] = lst[j + 1], lst[j]
-                draw_bars(draw_app, {j: draw_app.GREEN, j + i: draw_app.RED}, True)
+                draw_bars(draw_app, {j: draw_app.GREEN, j + 1: draw_app.RED}, True)
                 yield True
     return lst
-
-
-def sorting_algorithm():
-    pass
-
-
-def set_ascending(draw_app, ascending):
-    draw_app.ascending = ascending
 
 
 def main():
@@ -265,36 +250,37 @@ def main():
     # Set init sorting
     sorting = False
     # Create App object (Standard: ascending = True at initialization)
-    draw_app = DrawApp(600, 800, lst, ascending, sorting)
+    draw_app = DrawApp(600, 800, lst)
 
     objects = []
     bubble_button = Button(
-        0, 0, 150, 70, "Bubble", lambda: bubble_sort(draw_app), draw_app
+        0,
+        0,
+        150,
+        70,
+        "Bubble",
+        draw_app,
     )
-    insertion_button = Button(0, 71, 150, 70, "Insertion", button_press, draw_app)
-    merge_button = Button(0, 142, 150, 70, "Merge", button_press, draw_app)
+    insertion_button = Button(0, 71, 150, 70, "Insertion", draw_app)
+    merge_button = Button(0, 142, 150, 70, "Merge", draw_app)
 
-    ascending_button = Button(
-        650, 0, 150, 105, "Ascending", lambda: set_ascending(draw_app, True), draw_app
-    )
+    ascending_button = Button(650, 0, 150, 105, "Ascending", draw_app)
     descending_button = Button(
         650,
         107,
         150,
         105,
         "Descending",
-        lambda: set_ascending(draw_app, False),
         draw_app,
     )
 
-    start_button = Button(151, 142, 248, 70, "Start", button_press, draw_app)
+    start_button = Button(151, 142, 248, 70, "Start", draw_app)
     reset_button = Button(
         401,
         142,
         248,
         70,
         "Reset",
-        lambda: new_list(draw_app, n, min_val, max_val),
         draw_app,
     )
     objects.append(bubble_button)
@@ -307,26 +293,48 @@ def main():
     objects.append(start_button)
     objects.append(reset_button)
 
+    sorting_algorithm = bubble_sort
+    sorting_algo_name = "Bubble Sort"
+    sorting_algorithm_generator = None
+
     while run:
         # refresh at 60ticks per second
-        clock.tick(60)
-        # Draw to window
-        draw(draw_app, objects)
-        # Update window
-        pygame.display.update()
+        clock.tick(120)
+        if sorting:
+            try:
+                next(sorting_algorithm_generator)
+            except StopIteration:
+                sorting = False
+        else:
+            # Draw to window
+            draw(draw_app, objects, sorting_algo_name, ascending)
 
         # Listen for events
         for event in pygame.event.get():
             # If corner x is pressed exit loop
             if event.type == pygame.QUIT:
                 run = False
-            # Continue if random buttons are pressed down
-            if event.type != pygame.KEYDOWN:
-                continue
-            # Refresh list and set to object if the 'R' key is pressed
-            if event.key == pygame.K_r:
-                lst = create_list(n, min_val, max_val)
-                draw_app.set_list(lst)
+
+        if bubble_button.process():
+            sorting_algorithm = bubble_sort
+            sorting_algo_name = "Bubble Sort"
+        if merge_button.process():
+            print("Merge")
+        if insertion_button.process():
+            print("Insertion")
+        if reset_button.process():
+            lst = create_list(n, min_val, max_val)
+            draw_app.lst = lst
+            sorting = False
+        if start_button.process():
+            sorting = True
+            sorting_algorithm_generator = sorting_algorithm(draw_app, ascending)
+        if ascending_button.process():
+            ascending = True
+            sorting = False
+        if descending_button.process():
+            ascending = False
+            sorting = False
 
     pygame.quit()
 
